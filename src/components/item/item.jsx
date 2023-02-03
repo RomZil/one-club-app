9b733fbe00bc0cdd9adf639e3ca47a9ab5a5374c
RomZil <zilber.rom@gmail.com>
2023-02-03 21:29:18 +0200

    rom-create search - 3.8.2023

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './item.css';
+import React from "react";
+import { Card } from "react-bootstrap";
+import "./item.css";
 
-const Item = () => (
-  <div className="item">
-    Item Component
-  </div>
-);
-
-Item.propTypes = {};
-
-Item.defaultProps = {};
+function Item(props) {
+  return (
+    <Card onClick={props.nextFuncName} className="item" style={{ width: "30%", height: "30%" }}>
+      <Card.Img variant="top" src={props.img} />
+      <Card.Body>
+        <Card.Title>{props.text}</Card.Title>
+      </Card.Body>
+    </Card>
+  );
+}
 
 export default Item;
