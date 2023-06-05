91a65a31418e23915c3225d40525a3502ac4531d
adi unger <58906086+adiUngerGit@users.noreply.github.com>
2023-06-05 12:54:24 +0300

    add checkbox to myClubs

--- Client Side/src/pages/MyClubs/MyClubs.jsx
+++ Client Side/src/pages/MyClubs/MyClubs.jsx
@@ -1,5 +1,5 @@
 import "./MyClubs.css";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import Search from "../../components/search/search";
 import { Link } from "react-router-dom";
 import {
@@ -9,10 +9,21 @@ import {
 import { GET_DEALS } from "../../components/queries/dealQueries";
 import { useQuery } from "@apollo/client";
 import Spinner from "../../components/spinner/spinner";
+import { MDBCheckbox } from "mdb-react-ui-kit";
+import { useState } from "react";
 
 export default function Clubs() {
   const { loading, error, data } = useQuery(GET_LOYALTYCARDS);
+  const [myClubs, setMyClubs] = useState([]);
 
+  function updateMyClubs() {
+    //send the data to server
+    console.log(myClubs);
+  }
+
+  // function onClickCheckBox(name) {
+
+  // }
   if (loading) return <Spinner />;
   if (error) return <p>Somthing Went Wrong</p>;
 
@@ -23,10 +34,24 @@ export default function Clubs() {
         <Link id="link" to="/Profile">
           Go to your profile
         </Link>
+
         {data.loyaltyCards.map((loyaltyCard) => (
-          <div id="club">{loyaltyCard.name}</div>
+          <div
+            style={{
+              display: "contents",
+            }}
+          >
+            <div id="club">{loyaltyCard.name}</div>
+            <MDBCheckbox
+              onClick={() => setMyClubs([...myClubs, loyaltyCard.name])}
+              onClickname="flexCheck"
+              id="flexCheckDefault"
+            />
+          </div>
         ))}
       </Row>{" "}
+      <div>{""}</div>
+      <Button onClick={updateMyClubs}>UPDATE</Button>
     </Col>
   );
 }
