78eaa5c576ea61ea1be2e3206761852d80b942ef
adi unger <58906086+adiUngerGit@users.noreply.github.com>
2023-06-05 11:04:31 +0300

    fix small bugs

--- Client Side/src/App.js
+++ Client Side/src/App.js
@@ -5,19 +5,17 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Search from "./components/search/search";
 import Item from "./components/item/item";
 import { categories } from "../src/data/mockData.js";
-import Users from "./components/users/users"
+import Users from "./components/users/users";
 import { Col, Container, Row } from "react-bootstrap";
 import FilteresCategories from "./pages/FilteredCategories/FilteresCategories.jsx";
 import { AppRouter } from "./AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/footer/footer";
 
-
 const user = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: "http://localhost:3000/graphql",
   cache: new InMemoryCache(),
 });
-
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -27,17 +25,17 @@ const App = () => {
 
   return (
     <>
-    <Footer />
-    <br />
-    <ApolloProvider client={user}>
-      <BrowserRouter>
-        <div className="App">
-          <Container className="grid-items">
-            <AppRouter />
-          </Container>
-        </div>
-      </BrowserRouter>
-    </ApolloProvider>
+      <Footer />
+      <br />
+      <ApolloProvider client={user}>
+        <BrowserRouter>
+          <div className="App">
+            <Container className="grid-items">
+              <AppRouter />
+            </Container>
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
     </>
   );
 };
