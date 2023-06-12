8bc24215a1af0d735d27ae0547f8709e4004e19b
Ron Soffer <ronso008@gmail.com>
2023-06-12 18:59:40 +0300

    fix

--- Client Side/src/pages/MyClubs/MyClubs.jsx
+++ Client Side/src/pages/MyClubs/MyClubs.jsx
@@ -28,7 +28,6 @@ export default function MyClubs() {
       return { id: item };
     });
     cardsToSend = { cards: tmp };
-    debugger;
     updateUserLoyaltyCards({ variables: cardsToSend });
     // console.log(cards);
   }
