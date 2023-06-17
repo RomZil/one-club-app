772f29878b3372f73047e24c715e15652651879b
RomZil <zilber.rom@gmail.com>
2023-06-17 16:44:09 +0300

    fix token

--- Client Side/src/App.js
+++ Client Side/src/App.js
@@ -26,12 +26,14 @@ const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem("token");
   // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
+  if (token != null) {
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  }
 });
 
 const user = new ApolloClient({
