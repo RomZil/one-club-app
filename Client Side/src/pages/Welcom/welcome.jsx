b8878bc12985384bf2f5c66fbac3b1e71d2453ef
sapirmosko <72347902+sapirmosko@users.noreply.github.com>
2023-06-17 14:17:22 +0300

    ui welcome

--- Client Side/src/pages/Welcom/welcome.jsx
+++ Client Side/src/pages/Welcom/welcome.jsx
@@ -1,8 +1,78 @@
 import "./Welcome.css";
 import { useNavigate, Link } from "react-router-dom";
 import { AppRouter } from "../../AppRouter";
-import startImag from "../../images/Discount.gif";
+import startImag from "../../images/Discount.png";
 import { useEffect, useState } from "react";
+import food from "../../images/cimena.jpg";
+
+const categories = [
+  {
+    categoryTitle: "category",
+    img: food,
+  },
+  {
+    categoryTitle: "category",
+    img: food,
+  },
+  {
+    categoryTitle: "category",
+    img: food,
+  },
+  {
+    categoryTitle: "category",
+    img: food,
+  },
+  {
+    categoryTitle: "category",
+    img: food,
+  },
+  {
+    categoryTitle: "category",
+    img: food,
+  },
+  {
+    categoryTitle: "category",
+    img: food,
+  },
+];
+
+const items = [
+  {
+    itemTitle: "item",
+    itemDesc: "description",
+    img: food,
+  },
+  {
+    itemTitle: "item",
+    itemDesc: "description",
+    img: food,
+  },
+  {
+    itemTitle: "item",
+    itemDesc: "description",
+    img: food,
+  },
+  {
+    itemTitle: "item",
+    itemDesc: "description",
+    img: food,
+  },
+  {
+    itemTitle: "item",
+    itemDesc: "description",
+    img: food,
+  },
+  {
+    itemTitle: "item",
+    itemDesc: "description",
+    img: food,
+  },
+  {
+    itemTitle: "item",
+    itemDesc: "description",
+    img: food,
+  },
+];
 
 const Welcome = () => {
   const navigate = useNavigate();
@@ -15,6 +85,23 @@ const Welcome = () => {
   return (
     <div className="welcomeContainer">
       <img className="img" src={startImag} />
+      <div className="scrollItems">
+        {categories.map((item) => (
+          <div className="category">
+            <div className="categoryImg"></div>
+            <p>{item.categoryTitle}</p>
+          </div>
+        ))}
+      </div>
+      <div className="scrollItems2">
+        {items.map((item) => (
+          <div className="item2">
+            <img className="itemImg" src={food}></img>
+            <h3>{item.itemTitle}</h3>
+            <p>{item.itemDesc}</p>
+          </div>
+        ))}
+      </div>
       <button
         id="B_welcome"
         onClick={() => {
